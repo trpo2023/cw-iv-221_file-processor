731106b6b27414edc51be2a2390a261cf0c1f3b7
Diana Razgildeeva <bajidel@bajidel@mail.ru>
2023-06-03 13:08:27 +0700

    opendir

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include "rename.h"
-
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <string.h>
 int main(){
+
+    DIR* dir;
+    struct dirent *dp;
+    dir = opendir ("pp");
+    if(dir == NULL){
+        perror("Cannot open");
+        return 1;
+    }
+    while ((dp = readdir(dir)) != NULL) {
+             printf ("[%s]\n", dp->d_name);
+    }
+    char ras[]={'.txt', '.png'};
+    char newname[]= {'txt1', 'png1'};
+
+
+    closedir(dir);
+    return 0;
+
+    
     
 }
