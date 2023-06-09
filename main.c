bd73ba3b745a159f3b9856d91e4c0e79ee3aa81b
Andrei Dilman <AndrewDilman@users.noreply.github.com>
2023-06-09 15:13:26 +0700

    polish

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
-#include "rename.h"
+#include "opendir.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    
+int main()
+{
+    char adres[100];
+    DIR* dir = get_dir(adres);
+    if (!dir) {
+        return 1;
+    }
+    printf("YES\n");
+    closedir(dir);
+
+    return 0;
 }
---
+++ opendir.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+
+DIR* get_dir(char* dadres);
+int check_input(char* adres);
